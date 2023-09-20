7abd1535e7e209eeb60692d6dbbb04f9ec85f5da
Chenfeng Bao <chenfeng@chenfengbao.com>
2023-09-19 22:23:54 -0400

    debug

--- actions/configure/parse/parse.sh
+++ actions/configure/parse/parse.sh
@@ -22,6 +22,23 @@ echo "${CONFIG}" \
 			"provider_role_tfvar": $tfvar,
 			"environment": .environment,
 			"workspace_path": .path
+		}'
+
+echo "${CONFIG}" \
+	| yq -o=json . - \
+	| jq -cr '
+		.[]
+		| . as $account
+		| (.provider_role_arn_ro // "arn:aws:iam::\($account.account_id):role/terraform/tfp+github+'"${default_role_suffix}"'") as $provider_role_arn_ro
+		| (.provider_role_arn_rw // "arn:aws:iam::\($account.account_id):role/terraform/tfa+github+'"${default_role_suffix}"'") as $provider_role_arn_rw
+		| $account.workspaces[]
+		| (.provider_role_tfvar // $account.provider_role_tfvar // "terraform_role_arn") as $tfvar
+		| {
+			"provider_role_arn_ro": $provider_role_arn_ro,
+			"provider_role_arn_rw": $provider_role_arn_rw,
+			"provider_role_tfvar": $tfvar,
+			"environment": .environment,
+			"workspace_path": .path
 		}' \
 	 	- \
 	| xargs -d'\n' -I{} env ENVCONFIG='{}' "${HERE}/../configure.sh"
