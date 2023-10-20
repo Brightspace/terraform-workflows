af854f069654967eb51905ccd28fcfe79903c1e5
Chenfeng Bao <chenfeng.bao@d2l.com>
2023-10-20 09:31:39 -0400

    update backend role_arn format for Terraform 1.6+ (#42)

--- actions/script/init.sh
+++ actions/script/init.sh
@@ -12,11 +12,24 @@ onexit() {
 BACKEND_CONFIG=$(mktemp)
 cat > "${BACKEND_CONFIG}" << EOF
 region         = "us-east-1"
-role_arn       = "arn:aws:iam::891724658749:role/github/${GITHUB_REPOSITORY%/*}+${GITHUB_REPOSITORY#*/}+m"
 bucket         = "d2l-terraform-state"
 dynamodb_table = "d2l-terraform-state"
 key            = "github/${GITHUB_REPOSITORY}/${ENVIRONMENT}.tfstate"
 EOF
+
+MAJOR_VERSION=$(terraform version | grep -oP 'Terraform v\K\d+')
+MINOR_VERSION=$(terraform version | grep -oP 'Terraform v\d+\.\K\d+')
+if (( "${MAJOR_VERSION}" * 1000 + "${MINOR_VERSION}" >= 1006 )); then
+	cat >> "${BACKEND_CONFIG}" <<- EOF
+	assume_role = {
+	  role_arn = "arn:aws:iam::891724658749:role/github/${GITHUB_REPOSITORY%/*}+${GITHUB_REPOSITORY#*/}+m"
+	}
+	EOF
+else
+	cat >> "${BACKEND_CONFIG}" <<- EOF
+	role_arn       = "arn:aws:iam::891724658749:role/github/${GITHUB_REPOSITORY%/*}+${GITHUB_REPOSITORY#*/}+m"
+	EOF
+fi
 
 echo "##[group]terraform init"
 terraform init -input=false -backend-config="${BACKEND_CONFIG}"
